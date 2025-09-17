8fdee75a8f13c1a7acd87f3cc0066fb9b4d96171
a71143055 <a71143055@gmail.com>
2025-09-17 14:20:46 +0900

    PRJ_submission

---
+++ src/main/java/kr/ac/kopo/jeong/prj_submission/PrjSubmissionApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.jeong.prj_submission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjSubmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjSubmissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/jeong/prj_submission/PrjSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.jeong.prj_submission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjSubmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
